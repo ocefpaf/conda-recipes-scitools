b0281de13f24e574132178841ac05fa6c15a601f
Phil Elson <pelson.pub@gmail.com>
2014-09-23 23:04:41 +0100

    Forced a printout from cfchecker.

--- cfchecker/build.sh
+++ cfchecker/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+$PYTHON -c "import sysconfig; import os; l=sysconfig.get_config_var('LIBDIR'); print l; import glob; print sorted(glob.glob(l + '/*'))"
+
 $PYTHON -s setup.py install --prefix=$PREFIX
